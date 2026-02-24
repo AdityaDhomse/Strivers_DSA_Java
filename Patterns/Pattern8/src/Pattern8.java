6ac52f5f87b4d367402e53065673512df66d0086
Test Git <dhomseaditya@gmail.com>
2026-02-24 10:16:16 +0530

    Added Patterns

---
+++ Patterns/Pattern8/src/Pattern8.java
@@ -0,0 +1,36 @@
+
+public class Pattern8 {
+
+    public static void main(String[] args) {
+
+        /*
+        * * * * * * * * *
+          * * * * * * * 
+            * * * * * 
+              * * * 
+                *
+         */
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // space 
+            for (int j = 1; j < i; j++) {
+                System.out.print("   ");
+            }
+
+            // star
+            for (int j = 1; j <= 2 * (n - i) + 1; j++) {
+                System.out.print(" * ");
+            }
+
+            // space 
+            for (int j = 1; j < i; j++) {
+                System.out.print("   ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
