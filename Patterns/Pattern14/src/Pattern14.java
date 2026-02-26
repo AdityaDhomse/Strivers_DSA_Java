c0067d6d4da58d5494058c0b6d5e65accc1b41dd
Test Git <dhomseaditya@gmail.com>
2026-02-26 15:13:24 +0530

    Added Patterns

--- Patterns/Pattern14/src/Pattern14.java
+++ Patterns/Pattern14/src/Pattern14.java
@@ -1,5 +1,20 @@
 public class Pattern14 {
     public static void main(String[] args) {
-        
+        /*
+        A
+        AB
+        ABC
+        ABCD
+        ABCDE
+        */
+    
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (char j = 'A'; j < 'A' + i; j++) {
+                System.out.print(" " + j + " ");
+            }
+            System.out.println();
+        }
     }
 }
