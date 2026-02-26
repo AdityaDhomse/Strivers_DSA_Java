ed92e5bf9c4561d2c46b18bb0861c161d5ea60ea
Test Git <dhomseaditya@gmail.com>
2026-02-26 14:59:10 +0530

    Added Patterns

---
+++ Patterns/Pattern12/src/Pattern12.java
@@ -0,0 +1,38 @@
+
+public class Pattern12 {
+
+    public static void main(String[] args) {
+
+        /*
+        1                 1
+        1 2             2 1
+        1 2 3         3 2 1
+        1 2 3 4     4 3 2 1
+        1 2 3 4 5 5 4 3 2 1
+         */
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // space
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
